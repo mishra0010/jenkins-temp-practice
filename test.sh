28daa4543f32f5154e0947044aa09c0af6f1a95e
Harshit Mishra <88685324+mishra0010@users.noreply.github.com>
2025-09-01 19:17:07 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 echo "Running tests..."
-if [ $((RANDOM % 2)) -eq 0 ]; then
-  echo "✅ Tests passed!"
-  exit 0
-else
-  echo "❌ Tests failed!"
-  exit 1
-fi
+echo "Test complted succesfully"
+
